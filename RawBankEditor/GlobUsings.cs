5a8e121b50ef8db07c448fa3397ad123e20106b3
Filip Vateha <filip.vateha@student.tuke.sk>
2021-12-10 13:40:18 +0100

    refactoring + doc and string typos fixed

---
+++ RawBankEditor/GlobUsings.cs
@@ -0,0 +1,9 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Windows.Forms;
+global using System.Drawing;
+global using System.IO;
+global using System.ComponentModel;
+global using System.Diagnostics;
